817f47491b45cadef7fd81af7345bc97d69b7f0e
vikram khandelwal <vikramkhandelwal07@gmail.com>
2024-11-12 21:36:24 +0530

    add navbar and herosection

---
+++ tailwind.config.ts
@@ -0,0 +1,51 @@
+
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
+import type { Config } from "tailwindcss";
+
+// Define addVariablesForColors function first
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+  ],
+} satisfies Config;
